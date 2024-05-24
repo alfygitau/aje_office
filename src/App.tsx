ad67778e4c16aeff44dd7cd091a4695f0f3339ec
alfygitau <alfygitau@gmail.com>
2024-05-24 09:55:44 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className="text-[#f00]">zoza_office</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
